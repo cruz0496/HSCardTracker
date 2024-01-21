87bd894eb2c460cead4f4cd2c6e2b9834a676b04
Isai <icruz0496@gmail.com>
2024-01-20 18:29:26 -0600

    let's see what happens

---
+++ src/main/kotlin/com/hs/cruzstone/CruzstoneApplication.kt
@@ -0,0 +1,17 @@
+/*
+* All rights reserved by Austin McGowan, Isai Cruz, and the other brother.
+* This started as a silly project no one would ever see so this license shouldn't matter.
+*
+* Copyright 2024
+*/
+package com.hs.cruzstone
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CruzstoneApplication
+
+fun main(args: Array<String>) {
+    runApplication<CruzstoneApplication>(*args)
+}
---
+++ src/test/kotlin/com/hs/cruzstone/CruzstoneApplicationTests.kt
@@ -0,0 +1,17 @@
+/*
+* All rights reserved by Austin McGowan, Isai Cruz, and the other brother.
+* This started as a silly project no one would ever see so this license shouldn't matter.
+*
+* Copyright 2024
+*/
+package com.hs.cruzstone
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CruzstoneApplicationTests {
+    @Test
+    fun contextLoads() {
+    }
+}
